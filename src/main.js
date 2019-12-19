35f29f535e21fdaf1480419c5f968826a23496bc
timardex <timardex@gmail.com>
2019-12-19 19:10:37 +0100

    initial state setup

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-import './App.scss';
+/* Bootstrap */
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+import './App.scss'
 
 Vue.config.productionTip = false
 
