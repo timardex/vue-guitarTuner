fb58ebb9f27ed1e27c8421dfd5968d74ae325aa0
timardex <timardex@gmail.com>
2019-12-21 15:48:42 +0100

    working demo

--- src/components/Tuner/StringSelect.vue
+++ src/components/Tuner/StringSelect.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <ul class="string-list" v-if="selected === 'Standard Tuning E'">
-            <li :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in standards" :key="index">{{item.name}}</li>
+            <li @click="item.isPlaying ? pause(item, item.sound.pause(), item.sound.currentTime = 0) : play(item, item.sound.play())" :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in standards" :key="index">{{item.name}}</li>
         </ul>
         <ul class="string-list" v-else-if="selected === 'Tune down 1/2 step'">
-            <li :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_halfs" :key="index">{{item.name}}</li>
+            <li @click="item.isPlaying ? pause(item, item.sound.pause(), item.sound.currentTime = 0) : play(item, item.sound.play())" :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_halfs" :key="index">{{item.name}}</li>
         </ul>
         <ul class="string-list" v-else-if="selected === 'Tune down one step'">
-            <li :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_ones" :key="index">{{item.name}}</li>
+            <li @click="item.isPlaying ? pause(item, item.sound.pause(), item.sound.currentTime = 0) : play(item, item.sound.play())" :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_ones" :key="index">{{item.name}}</li>
         </ul>
         <ul class="string-list" v-else-if="selected === 'Tune down two step'">
-            <li :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_twos" :key="index">{{item.name}}</li>
+            <li @click="item.isPlaying ? pause(item, item.sound.pause(), item.sound.currentTime = 0) : play(item, item.sound.play())" :class="`${bounceIn} ${item.clicked}`" v-for="(item, index) in down_twos" :key="index">{{item.name}}</li>
         </ul>
     </div>
 </template>
@@ -35,6 +35,18 @@ export default {
         },
         down_twos() {
             return this.$store.state.initialState.down_twos
+        }
+    },
+    methods: {
+        pause: function (element) {
+            this.$store.dispatch('pause', element)
+        },
+        play: function (element) {
+            this.$store.dispatch('play', element)
+
+            setTimeout(() => {
+                this.$store.dispatch('animationEnd', element);
+            }, 8000)
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import initialState from './initialState'
+import {playSound, pauseSound, animationEnd} from './helpers'
 
 Vue.use(Vuex)
 
@@ -19,7 +20,26 @@ export default new Vuex.Store({
       state.initialState.animated_key = '';
       state.initialState.tuning = '';
       state.initialState.bounceIn = '';
-    }
+    },
+    PLAY: (state, payload) => {
+      state.initialState.standards = playSound(state.initialState.standards, payload);
+      state.initialState.down_halfs = playSound(state.initialState.down_halfs, payload);
+      state.initialState.down_ones = playSound(state.initialState.down_ones, payload);
+      state.initialState.down_twos = playSound(state.initialState.down_twos, payload);
+    },
+    PAUSE: (state, payload) => {
+      state.initialState.standards = pauseSound(state.initialState.standards, payload);
+      state.initialState.down_halfs = pauseSound(state.initialState.down_halfs, payload);
+      state.initialState.down_ones = pauseSound(state.initialState.down_ones, payload);
+      state.initialState.down_twos = pauseSound(state.initialState.down_twos, payload);
+    },
+    ANIMATION_END: (state, payload) => {
+      state.initialState.standards = animationEnd(state.initialState.standards, payload);
+      state.initialState.down_halfs = animationEnd(state.initialState.down_halfs, payload);
+      state.initialState.down_ones = animationEnd(state.initialState.down_ones, payload);
+      state.initialState.down_twos = animationEnd(state.initialState.down_twos, payload);
+    },
+
   },
   actions: {
     tuningSelect: (context, payload) => {
@@ -27,6 +47,15 @@ export default new Vuex.Store({
     },
     selectedTuned: (context) => {
       context.commit('SELECTED_TUNE')
+    },
+    pause: (context, payload) => {
+      context.commit('PAUSE', payload)
+    },
+    play: (context, payload) => {
+      context.commit('PLAY', payload)
+    },
+    animationEnd: (context, payload) => {
+      context.commit('ANIMATION_END', payload)
     }
   }
 })
